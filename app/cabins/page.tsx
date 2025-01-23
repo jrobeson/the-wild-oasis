15e865e309d9f2707694ded245f8c5594327ba22
Jon Robeson <jcbrobeson@gmail.com>
2025-01-23 15:29:49 -0600

    setting up server and client components

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,3 +1,23 @@
-export default function CabinsPage() {
-	return <h1>cabins page</h1>;
+import Counter from "../components/Counter";
+
+type User = {
+	id: string;
+	name: string;
+};
+
+export default async function CabinsPage() {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users');
+	const data: User[] = await res.json();
+
+	return (
+		<div>
+			<h1>Cabins Page</h1>
+			<ul>
+				{data.map((user) => (
+					<li key={user.id}>{user.name}</li>
+				))}
+			</ul>
+			<Counter />
+		</div>
+	);
 }
